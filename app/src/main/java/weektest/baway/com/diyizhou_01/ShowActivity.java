c44b8e5fe305152a6d4b0018118eae9fb2a9dd7d
lupeng001 <2295587818@qq.com>
2019-03-13 14:34:06 +0800

    Initial commit

---
+++ app/src/main/java/weektest/baway/com/diyizhou_01/ShowActivity.java
@@ -0,0 +1,18 @@
+package weektest.baway.com.diyizhou_01;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 作者;路鹏
+ * 时间：$date$ $time$
+ * 详细信息：
+ */
+public class ShowActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_activity);
+    }
+}
